f8558267061f8cf50658003e81ccc3599c8b72e7
Daniel Koch <danielkoch97@gmx.net>
2024-02-15 15:47:35 +0100

    Added new Petalinux Vitis Project with working connection between OS and Hardware but just the beginning

--- Petalinux/Petalinux_Platorm/platform.tcl
+++ Petalinux/Petalinux_Platorm/platform.tcl
@@ -50,3 +50,28 @@ domain active {zynq_fsbl}
 bsp reload
 bsp reload
 platform config -updatehw {/home/daniel/PetaLinuxProjects/ModuleTop.xsa}
+domain active {linux_domain}
+domain config -bif {}
+platform write
+domain config -boot {}
+platform write
+domain config -rootfs {}
+platform write
+domain config -image {}
+platform write
+platform generate
+platform generate
+platform active {Petalinux_Platorm}
+domain create -name {Standalone_APP} -os {standalone} -proc {ps7_cortexa9_0} -arch {32-bit} -display-name {Standalone_APP} -desc {} -runtime {cpp}
+platform generate -domains 
+platform write
+domain -report -json
+bsp reload
+platform generate -domains Standalone_APP 
+platform active {Petalinux_Platorm}
+domain active {zynq_fsbl}
+bsp reload
+domain active {Standalone_APP}
+bsp reload
+platform generate
+platform generate
